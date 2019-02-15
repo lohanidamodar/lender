fafa8a23366dbbe3766e9bf397b2766f3520e38e
Damodar Lohani <dlohani48@gmail.com>
2019-02-15 17:12:40 +0545

    implemented tabbar

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -3,9 +3,39 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Lender"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: Container(
+            padding: EdgeInsets.only(top: 40),
+            child: TabBar(
+              indicatorSize: TabBarIndicatorSize.label,
+              unselectedLabelColor: Colors.grey,
+              labelColor: Theme.of(context).primaryColor,
+              isScrollable: false,
+              tabs: <Widget>[
+                Tab(child: Text("Borrowed"),),
+                Tab(child: Text("Lent"),),
+                Tab(child: Text("Settings"),),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            Container(child: Text("Borrowed"),),
+            Container(child: Text("Lent"),),
+            Container(child: Text("Settings"),)
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){},
+        ),
       ),
     );
   }
