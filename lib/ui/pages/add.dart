8bfce9489be51269400377780accd122cd522bd3
Damodar Lohani <dlohani48@gmail.com>
2019-02-16 16:35:09 +0545

    form one

--- lib/ui/pages/add.dart
+++ lib/ui/pages/add.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:lender/ui/widgets/raised_button_widget.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
+  final String type;
+
+  const AddPage({Key key, this.type = "borrow"}) : super(key: key);
+
+  @override
+  _AddPageState createState() {
+    return new _AddPageState();
+  }
+}
+
+class _AddPageState extends State<AddPage> {
+  bool formOne;
+  String type;
+  String category;
+  final List<String> categories = ["Money", "Tech", "Books", "Games", "Clothes", "Others"];
+
+  @override
+  void initState() {
+    super.initState();
+    formOne = true;
+    type = widget.type;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,8 +33,68 @@ class AddPage extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(20.0),
-        child: Text("Add New Thing"),
+        child: _buildFormOne(context),
       )
     );
   }
+
+  void _changeType(String selectedType) {
+    setState(() {
+      type = selectedType;
+    });
+  }
+  
+  void _changeCategory(String selectedCategory) {
+    setState(() {
+      category = selectedCategory;
+    });
+  }
+
+  Widget _buildFormOne(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Type"),
+        SizedBox(height: 5.0,),
+        Wrap(
+          spacing: 10.0,
+          children: <Widget>[
+            PBRaisedButton(
+              color: type == 'borrow' ? Theme.of(context).primaryColor : Colors.grey.shade700,
+              child: Text("Borrow".toUpperCase()),
+              onPressed: () => _changeType('borrow'),
+            ),
+            PBRaisedButton(
+              color: type == 'lend' ? Theme.of(context).primaryColor : Colors.grey.shade700,
+              child: Text("Lend".toUpperCase()),
+              onPressed: () => _changeType('lend'),
+            ),
+          ],
+        ),
+        SizedBox(height: 20.0,),
+        Text("Category"),
+        SizedBox(height: 10.0,),
+        _buildCategories(),
+
+      ],
+    );
+  }
+
+  Widget _buildCategories() {
+    return Container(
+      height: 300,
+      child: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 3.5,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
+        children: categories.map((cat)=>PBRaisedButton(
+            color: category == cat ? Theme.of(context).primaryColor : Colors.grey.shade700,
+            child: Text(cat.toUpperCase()),
+            onPressed: () => _changeCategory(cat),
+          )).toList(),
+      ),
+    );
+  }
+
 }
--- lib/ui/widgets/raised_button_widget.dart
+++ lib/ui/widgets/raised_button_widget.dart
@@ -4,14 +4,16 @@ import 'package:flutter/material.dart';
 class PBRaisedButton extends StatelessWidget {
   final Widget child;
   final Function onPressed;
+  final Color color;
 
   PBRaisedButton({
-    Key key, @required this.child, @required this.onPressed,
+    Key key, @required this.child, @required this.onPressed, this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
+      color: color == null ? Theme.of(context).buttonTheme.colorScheme : color,
       textColor: Colors.white,
       onPressed: onPressed,
       child: child
